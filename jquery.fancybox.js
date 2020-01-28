5551bc757d0cf20c7e11e9708def7246434cb6a7
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-01-28 16:09:07 +0100

    trial

---
+++ jquery.fancybox.js
@@ -0,0 +1,7 @@
+$("[data-fancybox]").fancybox({
+  protect: true,
+  loop: true,
+  buttons: ["zoom", "slideShow", "thumbs", "close"],
+  animationEffect: "zoom-in-out",
+  transitionEffect: "slide"
+});
