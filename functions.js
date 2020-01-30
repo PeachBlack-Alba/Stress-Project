7c0cb2181b79a7a7f66a4ea185786a141bf5a0a4
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-01-30 16:21:48 +0100

    commit

--- functions.js
+++ functions.js
@@ -2,7 +2,6 @@
 
 //Books
 let books = [];
-
 function getAllBooks() {
   for (i = 0; i < data.books.length; i++) {
     books.push(data.books[i]);
@@ -37,4 +36,37 @@ function getAllDescription() {
 }
 getAllDescription();
 
-console.log(description);
+// Render cover
+
+function renderCover() {
+  for (var i = 0; i < books.length; i++) {
+    var title; //document..
+    var bookContainer = document.getElementById("bookContainer");
+
+    var div1 = document.createElement("div");
+    div1.className = "flip-card";
+
+    var div2 = document.createElement("div");
+    div2.className = "flip-card-inner";
+    div1.appendChild(div2);
+
+    var img = document.createElement("img");
+    img.setAttribute("src", books[i].cover);
+    div2.appendChild(img);
+
+    var div3 = document.createElement("div");
+    div3.className = "flip-card-back";
+
+    bookContainer.appendChild(div1);
+  }
+}
+renderCover();
+
+//Image Propierties
+
+function imgPropierty() {
+  var elem = books[i].cover;
+  elem.setAttribute("height", "200px");
+  elem.setAttribute("width", "200px");
+}
+imgPropierty();
